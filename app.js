5a251421dc5d6dad551d2a445d665d62cb886e4a
Dereje <getutadessedereje@icloud.com>
2022-04-04 13:44:54 +0200

    fin fonction random choice de l'ordi

--- app.js
+++ app.js
@@ -6,25 +6,169 @@
 // const b6 = document.querySelector('#ohhhhh');
     // b1, b2, b3, 
     // b4, b5, b6 
+const b = document.querySelector('#co');
+const b2 = document.querySelector('#ci');
+const b3 = document.querySelector('#cz');
 
-const tab = [ 
-    1,2,3,4,5,6,7,8,9
-];
+const tab = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 
-const end = [];
+const ordi = Math.floor(Math.random() * (3 - 0) +1);
 
-// function random() {
+for (let i = 0; i <= tab.length; i++) {
+    const div = document.createElement('div');
+    div.classList.add('jsDiv');
+    document.body.appendChild(div);
+}
 
-// };
+const supp = document.querySelectorAll('.jsDiv');
+console.log(supp);
+let i = 0;
+b.addEventListener('click', function () {
+    tab.splice(tab,1);
+    if (tab.length <= 1) {
+        alert('gg');
+    }else{ 
+        function time(){
+            const ordi = Math.floor(Math.random() * (3 - 0) +1);
+        
+            tab.splice(tab,ordi);
+            if (tab.length <= 1) {
+                alert('orfi');
+            }
+            if (ordi == 1) {
+                supp[i].classList.add('hidden');
+                i++
+            }else if(ordi == 2){
+                supp[i].classList.add('hidden');
+                i++
+                supp[i].classList.add('hidden');
+                i++
+            }else{
+                supp[i].classList.add('hidden');
+                i++
+                supp[i].classList.add('hidden');
+                i++
+                supp[i].classList.add('hidden');
+                i++
+            }
+            console.log(`choice ${ordi} ordi `);
+        }
+        setTimeout(time, 3000)
+    }
 
-let rand = tab[Math.floor(Math.random() * 3 )+1];
+    console.log(tab);
 
-end.push(rand);
+    supp[i].classList.add('hidden');
+    i++
 
-tab.splice(rand);
+});
 
-console.log(`${rand} random choice`);
 
-console.log(`${tab.length} tab joueurs`);
 
-console.log(`${end} tab de fin`);
+b2.addEventListener('click', function () {
+    tab.splice(tab,2);
+    if (tab.length <= 1) {
+        alert('gg');
+    }else{ 
+        function time(){
+            const ordi = Math.floor(Math.random() * (3 - 0) +1);
+        
+            tab.splice(tab,ordi);
+            if (tab.length <= 1) {
+                alert('orfi');
+            }
+            if (ordi == 1) {
+                supp[i].classList.add('hidden');
+                i++
+            }else if(ordi == 2){
+                supp[i].classList.add('hidden');
+                i++
+                supp[i].classList.add('hidden');
+                i++
+            }else{
+                supp[i].classList.add('hidden');
+                i++
+                supp[i].classList.add('hidden');
+                i++
+                supp[i].classList.add('hidden');
+                i++
+            }
+            console.log(tab);
+        }
+        setTimeout(time, 3000)
+    }
+    console.log(tab);
+    
+    supp[i].classList.add('hidden');
+    i++
+    supp[i].classList.add('hidden');
+    i++
+});
+
+b3.addEventListener('click', function () {
+    tab.splice(tab,3);
+    if (tab.length <= 1) {
+        alert('gg');
+    }else{ 
+        function time(){
+            const ordi = Math.floor(Math.random() * (3 - 0) +1);
+        
+            tab.splice(tab,ordi);
+            if (tab.length <= 1) {
+                alert('orfi');
+            }
+            if (ordi == 1) {
+                supp[i].classList.add('hidden');
+                i++
+            }else if(ordi == 2){
+                supp[i].classList.add('hidden');
+                i++
+                supp[i].classList.add('hidden');
+                i++
+            }else{
+                supp[i].classList.add('hidden');
+                i++
+                supp[i].classList.add('hidden');
+                i++
+                supp[i].classList.add('hidden');
+                i++
+            }
+            console.log(tab);
+        }
+        setTimeout(time, 3000)
+
+    }
+    // tab.splice(tab,ordi);
+    console.log(tab);
+    supp[i].classList.add('hidden');
+    i++
+    supp[i].classList.add('hidden');
+    i++
+    supp[i].classList.add('hidden');
+    i++
+});
+
+// if(tab.length == 0){
+//     console.log('coucou');
+// }
+
+
+
+
+// tab.splice(tab,3);
+
+// tab.splice(tab,2);
+// tab.splice(tab,1);
+
+
+
+
+console.log(`${tab.length} tab du joueur`);
+
+
+console.log(tab);
+
+
+
+
+
