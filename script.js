ee98af3b4f4e69e56d6adda0e4913627bc0f2549
Moulun Florian <flomoulun@gmx.fr>
2022-04-04 16:46:14 +0200

    up

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-document.querySelector('button').addEventListener('click', function(e) {
-  document.querySelector('div').classList.toggle('animate');
-})
-
 const b = document.querySelector('#co');
 const b2 = document.querySelector('#ci');
 const b3 = document.querySelector('#cz');
+const maindiv = document.querySelector('#maindiv');
+const question = document.querySelector('#question');
+const question2 = document.querySelector('#question2');
+const hiddendiv = document.querySelector('#hiddendiv')
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2')
+const button3 = document.querySelector('#button3')
+const button4 = document.querySelector('#button4')
 
+
+question.addEventListener('click', function(){
+  question.style.display= "none";
+  question2.style.display= "flex";
+  hiddendiv.style.display = "flex";
+  button1.style.display = "none";
+  button2.style.display = "none";
+  button3.style.display = "none";
+  button4.style.display = "none";
+})
+
+question2.addEventListener('click', function(){
+  question.style.display="flex";
+  question2.style.display= "none";
+  hiddendiv.style.display = "none";
+  button1.style.display = "flex";
+  button2.style.display = "flex";
+  button3.style.display = "flex";
+  button4.style.display = "flex";
+})
 const tab = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 
 const ordi = Math.floor(Math.random() * (3 - 0) +1);
@@ -13,15 +37,20 @@ const ordi = Math.floor(Math.random() * (3 - 0) +1);
 for (let i = 0; i < tab.length; i++) {
     const div = document.createElement('div');
     div.classList.add('jsDiv');
-    document.body.appendChild(div);
+    maindiv.appendChild(div);
 }
 
 const supp = document.querySelectorAll('.jsDiv');
 console.log(supp);
 let i = 0;
+
 b.addEventListener('click', function () {
+
     tab.splice(tab,1);
-    if (tab.length <= 1) {
+    if (tab.length == 1) {
+       b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
         alert('gg');
     }else{ 
       if (tab.length == 4) {
@@ -29,7 +58,10 @@ b.addEventListener('click', function () {
             const ordi = 3;
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+               b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -48,19 +80,26 @@ b.addEventListener('click', function () {
                 supp[i].classList.add('hidden');
                 i++
             }
+
             console.log(`choice ${ordi} ordi `);
+            
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     
 
 
       }
        else if (tab.length == 3) {
+   
+  
         function time(){
             const ordi = 2;
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+               b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -81,14 +120,23 @@ b.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     }
     else if (tab.length == 2) {
+
+              
+              b2.style.display="none"
+              b3.style.display="none"
+      
       function time(){
             const ordi = 1;
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+               b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
+
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -109,7 +157,7 @@ b.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     }
 
     else {
@@ -117,7 +165,10 @@ b.addEventListener('click', function () {
             const ordi = Math.floor(Math.random() * (3 - 0) +1);
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+               b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -138,7 +189,7 @@ b.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     }
 
     }
@@ -154,7 +205,10 @@ b.addEventListener('click', function () {
 
 b2.addEventListener('click', function () {
     tab.splice(tab,2);
-    if (tab.length <= 1) {
+    if (tab.length == 1) {
+       b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
         alert('gg');
     }else{ 
       if (tab.length == 4) {
@@ -162,7 +216,10 @@ b2.addEventListener('click', function () {
             const ordi = 3;
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+              b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -183,17 +240,23 @@ b2.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     
 
 
       }
        else if (tab.length == 3) {
+
+              
+              b3.style.display="none"
         function time(){
             const ordi = 2;
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+               b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -214,14 +277,21 @@ b2.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     }
     else if (tab.length == 2) {
+
+              
+              b2.style.display="none"
+              b3.style.display="none"
       function time(){
             const ordi = 1;
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+              b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -242,7 +312,7 @@ b2.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     }
 
     else {
@@ -250,7 +320,10 @@ b2.addEventListener('click', function () {
             const ordi = Math.floor(Math.random() * (3 - 0) +1);
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+              b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -271,7 +344,7 @@ b2.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     }
 
     }
@@ -286,16 +359,23 @@ b2.addEventListener('click', function () {
 });
 
 b3.addEventListener('click', function () {
+  
     tab.splice(tab,3);
-    if (tab.length <= 1) {
+    if (tab.length == 1) {
+       b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
         alert('gg');
-    }else{ 
+    }else { 
       if (tab.length == 4) {
         function time(){
             const ordi = 3;
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+              b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -316,18 +396,24 @@ b3.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     
 
 
       }
        else if (tab.length == 3) {
+
+              
+              b3.style.display="none"
         function time(){
             const ordi = 2;
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
                 alert('orfi');
+                 b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
             }
             if (ordi == 1) {
                 supp[i].classList.add('hidden');
@@ -347,15 +433,24 @@ b3.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     }
     else if (tab.length == 2) {
+
+             
+              b2.style.display="none"
+              b3.style.display="none"
       function time(){
             const ordi = 1;
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+
+             b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
+          
             }
             if (ordi == 1) {
                 supp[i].classList.add('hidden');
@@ -375,7 +470,7 @@ b3.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     }
 
     else {
@@ -383,7 +478,10 @@ b3.addEventListener('click', function () {
             const ordi = Math.floor(Math.random() * (3 - 0) +1);
         
             tab.splice(tab,ordi);
-            if (tab.length <= 1) {
+            if (tab.length == 1) {
+              b.style.display="none";
+              b2.style.display="none"
+              b3.style.display="none"
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -394,7 +492,7 @@ b3.addEventListener('click', function () {
                 i++
                 supp[i].classList.add('hidden');
                 i++
-            }else{
+            }else {
                 supp[i].classList.add('hidden');
                 i++
                 supp[i].classList.add('hidden');
@@ -402,9 +500,9 @@ b3.addEventListener('click', function () {
                 supp[i].classList.add('hidden');
                 i++
             }
-            console.log(`choice ${ordi} ordi `);
+            console.log(`${ordi} est le choix de l'ordi `);
         }
-        setTimeout(time, 2000)
+        setTimeout(time, 1500)
     }
 
     }
@@ -424,7 +522,7 @@ b3.addEventListener('click', function () {
 
 
 
-console.log(`${tab.length} tab du joueur`);
+console.log(`${tab.length} est taille du tableaux`);
 
 
 console.log(tab);
