dad3fa2295f84d2aa92abed7552ed24eef32b883
Dereje <95926729+Dereje-getu-tadesse-10@users.noreply.github.com>
2022-04-05 11:41:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,174 +1,31 @@
+
 const b = document.querySelector('#co');
 const b2 = document.querySelector('#ci');
 const b3 = document.querySelector('#cz');
-const maindiv = document.querySelector('#maindiv');
-const question = document.querySelector('#question');
-const question2 = document.querySelector('#question2');
-const hiddendiv = document.querySelector('#hiddendiv')
-const button1 = document.querySelector('#button1');
-const button2 = document.querySelector('#button2')
-const button3 = document.querySelector('#button3')
-const button4 = document.querySelector('#button4')
 
-
-question.addEventListener('click', function(){
-  question.style.display= "none";
-  question2.style.display= "flex";
-  hiddendiv.style.display = "flex";
-  button1.style.display = "none";
-  button2.style.display = "none";
-  button3.style.display = "none";
-  button4.style.display = "none";
-})
-
-question2.addEventListener('click', function(){
-  question.style.display="flex";
-  question2.style.display= "none";
-  hiddendiv.style.display = "none";
-  button1.style.display = "flex";
-  button2.style.display = "flex";
-  button3.style.display = "flex";
-  button4.style.display = "flex";
-})
 const tab = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 
 const ordi = Math.floor(Math.random() * (3 - 0) +1);
 
-for (let i = 0; i < tab.length; i++) {
+for (let i = 0; i <= tab.length; i++) {
     const div = document.createElement('div');
     div.classList.add('jsDiv');
-    maindiv.appendChild(div);
+    document.body.appendChild(div);
 }
 
 const supp = document.querySelectorAll('.jsDiv');
 console.log(supp);
 let i = 0;
-
 b.addEventListener('click', function () {
-
     tab.splice(tab,1);
-    if (tab.length == 1) {
-       b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
+    if (tab.length <= 1) {
         alert('gg');
     }else{ 
-      if (tab.length == 4) {
         function time(){
-            const ordi = 3;
-        
-            tab.splice(tab,ordi);
-            if (tab.length == 1) {
-               b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
-                alert('orfi');
-            }
-            if (ordi == 1) {
-                supp[i].classList.add('hidden');
-                i++
-            }else if(ordi == 2){
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }else{
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }
-
-            console.log(`choice ${ordi} ordi `);
-            
-        }
-        setTimeout(time, 1500)
-    
-
-
-      }
-       else if (tab.length == 3) {
-   
-  
-        function time(){
-            const ordi = 2;
-        
-            tab.splice(tab,ordi);
-            if (tab.length == 1) {
-               b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
-                alert('orfi');
-            }
-            if (ordi == 1) {
-                supp[i].classList.add('hidden');
-                i++
-            }else if(ordi == 2){
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }else{
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }
-            console.log(`choice ${ordi} ordi `);
-        }
-        setTimeout(time, 1500)
-    }
-    else if (tab.length == 2) {
-
-              
-              b2.style.display="none"
-              b3.style.display="none"
-      
-      function time(){
-            const ordi = 1;
-        
-            tab.splice(tab,ordi);
-            if (tab.length == 1) {
-               b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
-
-                alert('orfi');
-            }
-            if (ordi == 1) {
-                supp[i].classList.add('hidden');
-                i++
-            }else if(ordi == 2){
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }else{
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }
-            console.log(`choice ${ordi} ordi `);
-        }
-        setTimeout(time, 1500)
-    }
-
-    else {
-      function time(){
             const ordi = Math.floor(Math.random() * (3 - 0) +1);
         
             tab.splice(tab,ordi);
-            if (tab.length == 1) {
-               b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
+            if (tab.length <= 1) {
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -189,11 +46,9 @@ b.addEventListener('click', function () {
             }
             console.log(`choice ${ordi} ordi `);
         }
-        setTimeout(time, 1500)
+        setTimeout(time, 3000)
     }
 
-    }
-        
     console.log(tab);
 
     supp[i].classList.add('hidden');
@@ -205,125 +60,14 @@ b.addEventListener('click', function () {
 
 b2.addEventListener('click', function () {
     tab.splice(tab,2);
-    if (tab.length == 1) {
-       b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
+    if (tab.length <= 1) {
         alert('gg');
     }else{ 
-      if (tab.length == 4) {
         function time(){
-            const ordi = 3;
-        
-            tab.splice(tab,ordi);
-            if (tab.length == 1) {
-              b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
-                alert('orfi');
-            }
-            if (ordi == 1) {
-                supp[i].classList.add('hidden');
-                i++
-            }else if(ordi == 2){
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }else{
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }
-            console.log(`choice ${ordi} ordi `);
-        }
-        setTimeout(time, 1500)
-    
-
-
-      }
-       else if (tab.length == 3) {
-
-              
-              b3.style.display="none"
-        function time(){
-            const ordi = 2;
-        
-            tab.splice(tab,ordi);
-            if (tab.length == 1) {
-               b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
-                alert('orfi');
-            }
-            if (ordi == 1) {
-                supp[i].classList.add('hidden');
-                i++
-            }else if(ordi == 2){
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }else{
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }
-            console.log(`choice ${ordi} ordi `);
-        }
-        setTimeout(time, 1500)
-    }
-    else if (tab.length == 2) {
-
-              
-              b2.style.display="none"
-              b3.style.display="none"
-      function time(){
-            const ordi = 1;
-        
-            tab.splice(tab,ordi);
-            if (tab.length == 1) {
-              b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
-                alert('orfi');
-            }
-            if (ordi == 1) {
-                supp[i].classList.add('hidden');
-                i++
-            }else if(ordi == 2){
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }else{
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }
-            console.log(`choice ${ordi} ordi `);
-        }
-        setTimeout(time, 1500)
-    }
-
-    else {
-      function time(){
             const ordi = Math.floor(Math.random() * (3 - 0) +1);
         
             tab.splice(tab,ordi);
-            if (tab.length == 1) {
-              b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
+            if (tab.length <= 1) {
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -342,146 +86,28 @@ b2.addEventListener('click', function () {
                 supp[i].classList.add('hidden');
                 i++
             }
-            console.log(`choice ${ordi} ordi `);
+            console.log(tab);
         }
-        setTimeout(time, 1500)
+        setTimeout(time, 3000)
     }
-
-    }
-        
     console.log(tab);
-
+    
     supp[i].classList.add('hidden');
     i++
     supp[i].classList.add('hidden');
     i++
-
 });
 
 b3.addEventListener('click', function () {
-  
     tab.splice(tab,3);
-    if (tab.length == 1) {
-       b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
+    if (tab.length <= 1) {
         alert('gg');
-    }else { 
-      if (tab.length == 4) {
+    }else{ 
         function time(){
-            const ordi = 3;
-        
-            tab.splice(tab,ordi);
-            if (tab.length == 1) {
-              b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
-                alert('orfi');
-            }
-            if (ordi == 1) {
-                supp[i].classList.add('hidden');
-                i++
-            }else if(ordi == 2){
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }else{
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }
-            console.log(`choice ${ordi} ordi `);
-        }
-        setTimeout(time, 1500)
-    
-
-
-      }
-       else if (tab.length == 3) {
-
-              
-              b3.style.display="none"
-        function time(){
-            const ordi = 2;
-        
-            tab.splice(tab,ordi);
-            if (tab.length == 1) {
-                alert('orfi');
-                 b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
-            }
-            if (ordi == 1) {
-                supp[i].classList.add('hidden');
-                i++
-            }else if(ordi == 2){
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }else{
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }
-            console.log(`choice ${ordi} ordi `);
-        }
-        setTimeout(time, 1500)
-    }
-    else if (tab.length == 2) {
-
-             
-              b2.style.display="none"
-              b3.style.display="none"
-      function time(){
-            const ordi = 1;
-        
-            tab.splice(tab,ordi);
-            if (tab.length == 1) {
-
-             b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
-                alert('orfi');
-          
-            }
-            if (ordi == 1) {
-                supp[i].classList.add('hidden');
-                i++
-            }else if(ordi == 2){
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }else{
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-                supp[i].classList.add('hidden');
-                i++
-            }
-            console.log(`choice ${ordi} ordi `);
-        }
-        setTimeout(time, 1500)
-    }
-
-    else {
-      function time(){
             const ordi = Math.floor(Math.random() * (3 - 0) +1);
         
             tab.splice(tab,ordi);
-            if (tab.length == 1) {
-              b.style.display="none";
-              b2.style.display="none"
-              b3.style.display="none"
+            if (tab.length <= 1) {
                 alert('orfi');
             }
             if (ordi == 1) {
@@ -492,7 +118,7 @@ b3.addEventListener('click', function () {
                 i++
                 supp[i].classList.add('hidden');
                 i++
-            }else {
+            }else{
                 supp[i].classList.add('hidden');
                 i++
                 supp[i].classList.add('hidden');
@@ -500,29 +126,56 @@ b3.addEventListener('click', function () {
                 supp[i].classList.add('hidden');
                 i++
             }
-            console.log(`${ordi} est le choix de l'ordi `);
+            console.log(tab);
         }
-        setTimeout(time, 1500)
-    }
+        setTimeout(time, 3000)
 
     }
-        
+    // tab.splice(tab,ordi);
     console.log(tab);
-
     supp[i].classList.add('hidden');
     i++
     supp[i].classList.add('hidden');
     i++
-     supp[i].classList.add('hidden');
+    supp[i].classList.add('hidden');
     i++
-
 });
 
 
-
-
-
-console.log(`${tab.length} est taille du tableaux`);
+console.log(`${tab.length} tab du joueur`);
 
 
 console.log(tab);
+
+
+
+const b1 = document.querySelector('#a');
+const b2 = document.querySelector('#z');
+const b3 = document.querySelector('#e');
+const b4 = document.querySelector('#r');
+const b5 = document.querySelector('#t');
+const b6 = document.querySelector('#y');
+
+b1.addEventListener('click', () => {
+    
+})
+
+const tab = [
+    b1,b2,b3,
+    b4,b5,b6
+];
+
+const fin = []; 
+
+function d() {
+    let rdm = tab[Math.floor(Math.random() * 3 )]
+    fin.push(rdm)
+    tab.splice(rdm)
+    return rdm
+}
+
+console.log(fin);
+console.log(tab);
+
+console.log(d());
+
